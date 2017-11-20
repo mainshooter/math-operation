862cbd760716ca436233ac28d312b2584cde2d07
Peter 4A5A <498883@edu.rocmn.nl>
2017-11-20 13:28:38 +0100

    Added documentation

--- Math.class.php
+++ Math.class.php
@@ -9,18 +9,32 @@
       $this->secondValue = null;
     }
 
+    /**
+     * Sets the first value of the math we will do
+     */
     protected function setFirstValue($value) {
       $this->firstValue = $value;
     }
 
+    /**
+     * Returns the first setted value
+     * @return [float] [The first value of the math]
+     */
     public function getFirstValue() {
       return($this->firstValue);
     }
 
+    /**
+     * Sets the second vlaue of our math
+     */
     protected function setSecondValue($value) {
       $this->secondValue = $value;
     }
 
+    /**
+     * Returns the second value that we had set
+     * @return [float] [The second value of our math]
+     */
     public function getSecondValue() {
       return($this->secondValue);
     }
--- Operation.class.php
+++ Operation.class.php
@@ -4,6 +4,11 @@
   class Operation extends Math {
     private $operationResult;
 
+    /**
+     * Runs on defining the class and sets the first and second value
+     * @param [float] $firstValue  [The first value]
+     * @param [float] $secondValue [The second value]
+     */
     public function __construct($firstValue, $secondValue) {
       $this->operationResult = null;
 
@@ -11,11 +16,19 @@
       $this->setSecondValue($secondValue);
     }
 
+    /**
+     * Substract the first from the second setted value
+     * @return [float] [The result of the math]
+     */
     public function subTract() {
       $this->operationResult = $this->getFirstValue() - $this->getSecondValue();
       return($this->operationResult);
     }
 
+    /**
+     * Ads up the first and the second value and returns if
+     * @return [float] [The result from the math]
+     */
     public function addUp() {
       $this->operationResult = $this->getFirstValue() + $this->getSecondValue();
       return($this->operationResult);
