efb80fc94f61b282ba350140f26e896a6db6677a
Peter 4A5A <498883@edu.rocmn.nl>
2017-11-20 13:01:43 +0100

    wrote basic class

---
+++ Math.class.php
@@ -0,0 +1,30 @@
+<?php
+
+  class Math {
+    private $firstValue;
+    private $secondValue;
+
+    public function __construct() {
+      $this->firstValue = null;
+      $this->secondValue = null;
+    }
+
+    public function setFirstValue($value) {
+      $this->firstValue = $value;
+    }
+
+    public function getFirstValue() {
+      return($this->firstValue);
+    }
+
+    public function setSecondValue($value) {
+      $this->secondValue = $value;
+    }
+
+    public function getSecondValue() {
+      return($this->secondValue);
+    }
+  }
+
+
+?>
